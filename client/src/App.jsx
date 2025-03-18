d05db6a17819a35cc7870067ba60e1c0af53e832
om-ghante <omghante.official@gmail.com>
2025-03-18 22:09:45 +0530

    React+vite and Tailwind Css Setup Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className='text-2xl text-center text-red-800 py-10'
+      >
+        Hello, World!
+      </h1>
+    </>
+  )
+}
+
+export default App
